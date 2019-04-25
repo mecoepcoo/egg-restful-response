964fffa5777211c4eb5bdc607a4d3076f73e8520
Tianzhen <340349237@qq.com>
2019-04-25 23:17:28 +0800

    完成方法编写

--- app/extend/context.js
+++ app/extend/context.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const code = {
-  OK: 200, // [GET] 服务器成功返回用户请求的数据，该操作是幂等的
+  OK: 200, // [GET] 服务器成功返回用户请求的数据
   CREATED: 201, // [POST/PUT/PATCH] 用户新建或修改数据成功
   ACCEPTED: 202, // 一个请求已经进入后台排队（异步任务）
   NO_CONTENT: 204, // [DELETE] 删除数据成功
@@ -26,7 +26,7 @@ function successBody(data, extend) {
 
 function failBody(config, msg, cause, data, err) {
   let body;
-  if (config.restfulResponse.showError) {
+  if (config.restfulResponse && config.restfulResponse.showError) {
     body = {
       msg,
       cause,
@@ -44,13 +44,181 @@ function failBody(config, msg, cause, data, err) {
 }
 
 module.exports = {
+  /**
+   * [GET] 服务器成功返回用户请求的数据
+   * @param {Object} body 响应体
+   * @param {Object} body.data 数据信息
+   * @param {Object} body.extend 额外信息
+   */
   sendOk({ data = {}, extend = {} } = {}) {
     this.status = code.OK;
     this.body = successBody(data, extend);
   },
 
+  /**
+   * [POST|PUT|PATCH] 新建或修改数据成功
+   * @param {Object} body 响应体
+   * @param {Object} body.data 数据信息
+   * @param {Object} body.extend 额外信息
+   */
+  sendCreated({ data = {}, extend = {} } = {}) {
+    this.status = code.CREATED;
+    this.body = successBody(data, extend);
+  },
+
+  /**
+   * 一个请求已经进入后台排队（异步任务）
+   * @param {Object} body 响应体
+   * @param {Object} body.data 数据信息
+   * @param {Object} body.extend 额外信息
+   */
+  sendAccepted({ data = {}, extend = {} } = {}) {
+    this.status = code.ACCEPTED;
+    this.body = successBody(data, extend);
+  },
+
+  /**
+   * [DELETE] 删除数据成功
+   * @param {Object} body 响应体
+   * @param {Object} body.data 数据信息
+   * @param {Object} body.extend 额外信息
+   */
+  sendNoContent({ data = {}, extend = {} } = {}) {
+    this.status = code.NO_CONTENT;
+    this.body = successBody(data, extend);
+  },
+
+  /**
+   * [POST|PUT|DELETE] 暂时重定向，由客户端决定
+   * @param {Object} body 响应体
+   * @param {String} body.url 建议用户向此 url 重定向
+   * @param {Object} body.extend 额外信息
+   */
+  sendSeeOthoer({ url = '', extend = {} }) {
+    this.status = code.SEE_OTHER;
+    this.body = {
+      data: {
+        url,
+      },
+      extend,
+    };
+  },
+
+  /**
+   * [POST|PUT|PATCH] 服务器不理解客户端的请求，未做任何处理
+   * @param {Object} body 响应体
+   * @param {String} body.msg 错误文本信息
+   * @param {String} body.cause 错误原因，或错误发生的大概位置
+   * @param {Object} body.data 其他信息
+   * @param {Error} err 错误对象，默认情况下不在生产环境输出
+   */
   sendBadRequest({ msg = '', cause = '', data = {} } = {}, err) {
     this.status = code.BAD_REQUEST;
     this.body = failBody(this.app.config, msg, cause, data, err);
   },
+
+  /**
+   * 用户未提供身份验证凭据，或者没有通过身份验证
+   * @param {Object} body 响应体
+   * @param {String} body.msg 错误文本信息
+   * @param {String} body.cause 错误原因，或错误发生的大概位置
+   * @param {Object} body.data 其他信息
+   * @param {Error} err 错误对象，默认情况下不在生产环境输出
+   */
+  sendUnauthorized({ msg = '', cause = '', data = {} } = {}, err) {
+    this.status = code.UNAUTHORIZED;
+    this.body = failBody(this.app.config, msg, cause, data, err);
+  },
+
+  /**
+   * 用户通过了身份验证，但是不具有访问资源所需的权限
+   * @param {Object} body 响应体
+   * @param {String} body.msg 错误文本信息
+   * @param {String} body.cause 错误原因，或错误发生的大概位置
+   * @param {Object} body.data 其他信息
+   * @param {Error} err 错误对象，默认情况下不在生产环境输出
+   */
+  sendForbidden({ msg = '', cause = '', data = {} } = {}, err) {
+    this.status = code.FORBIDDEN;
+    this.body = failBody(this.app.config, msg, cause, data, err);
+  },
+
+  /**
+   * 所请求的资源不存在或不可用
+   * @param {Object} body 响应体
+   * @param {String} body.msg 错误文本信息
+   * @param {String} body.cause 错误原因，或错误发生的大概位置
+   * @param {Object} body.data 其他信息
+   * @param {Error} err 错误对象，默认情况下不在生产环境输出
+   */
+  sendNotFound({ msg = '', cause = '', data = {} } = {}, err) {
+    this.status = code.NOT_FOUND;
+    this.body = failBody(this.app.config, msg, cause, data, err);
+  },
+
+  /**
+   * [GET] 不支持用户请求的格式，如请求json，但只有xml
+   * @param {Object} body 响应体
+   * @param {String} body.msg 错误文本信息
+   * @param {String} body.cause 错误原因，或错误发生的大概位置
+   * @param {Object} body.data 其他信息
+   * @param {Error} err 错误对象，默认情况下不在生产环境输出
+   */
+  sendNotAcceptable({ msg = '', cause = '', data = {} } = {}, err) {
+    this.status = code.NOT_ACCEPTABLE;
+    this.body = failBody(this.app.config, msg, cause, data, err);
+  },
+
+  /**
+   * [GET] 所请求的资源已被永久转移，不再可用
+   * @param {Object} body 响应体
+   * @param {String} body.msg 错误文本信息
+   * @param {String} body.cause 错误原因，或错误发生的大概位置
+   * @param {Object} body.data 其他信息
+   * @param {Error} err 错误对象，默认情况下不在生产环境输出
+   */
+  sendGone({ msg = '', cause = '', data = {} } = {}, err) {
+    this.status = code.GONE;
+    this.body = failBody(this.app.config, msg, cause, data, err);
+  },
+
+  /**
+   * [POST|PUT|PATCH] 客户端提交的表单验证失败
+   * @param {Object} body 响应体
+   * @param {String} body.msg 错误文本信息
+   * @param {String} body.cause 错误原因，或错误发生的大概位置
+   * @param {Object} body.data 其他信息
+   * @param {Error} err 错误对象，默认情况下不在生产环境输出
+   */
+  sendUnprocesableEntity({ msg = '', cause = '', data = {} } = {}, err) {
+    this.status = code.UNPROCESABLE_ENTITY;
+    this.body = failBody(this.app.config, msg, cause, data, err);
+  },
+
+  /**
+   * 服务端发生错误
+   * @param {Object} body 响应体
+   * @param {String} body.msg 错误文本信息
+   * @param {String} body.cause 错误原因，或错误发生的大概位置
+   * @param {Object} body.data 其他信息
+   * @param {Error} err 错误对象，默认情况下不在生产环境输出
+   */
+  sendInternalServerError({ msg = '', cause = '', data = {} } = {}, err) {
+    this.status = code.INTERNAL_SERVER_ERROR;
+    this.body = failBody(this.app.config, msg, cause, data, err);
+  },
+
+  /**
+   * 服务暂时不可用
+   * @param {Object} body 响应体
+   * @param {String} body.msg 错误文本信息
+   * @param {String} body.cause 错误原因，或错误发生的大概位置
+   * @param {Object} body.data 其他信息
+   * @param {Error} err 错误对象，默认情况下不在生产环境输出
+   */
+  sendServiceUnavailable({ msg = '', cause = '', data = {} } = {}, err) {
+    this.status = code.SERVICE_UNAVAILABLE;
+    this.body = failBody(this.app.config, msg, cause, data, err);
+  },
+
 };
--- test/fixtures/apps/restful-response-test/config/config.default.js
+++ test/fixtures/apps/restful-response-test/config/config.default.js
@@ -1,3 +1,7 @@
 'use strict';
 
 exports.keys = '123456';
+
+exports.restfulResponse = {
+  showError: true, // 显示具体错误信息
+};
